3a36c405fafdfdb7a2837e59be9d19d675c34124
Sahil Naik <81952895+Sahil-Naik@users.noreply.github.com>
2021-10-14 11:26:34 +0530

    Create Matrix_Operation.py

---
+++ Matrix_Operation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+matx = np.array([[1,2],[5,4]])
+maty = np.array([[0,3],[7,1]])
+
+print("Given Matrix are :\n",matx,"\n\n",maty)
+print("\nAddition of given matrix is:\n", np.add(matx,maty))
+print("\nSubtraction of given matrix is:\n", np.subtract(matx,maty))
+print("\nMultiplication of given matrix is:\n", np.multiply(matx,maty))
+print("\nTranspose of given matrix are:\n", matx.T, "\n\n", maty.T)
