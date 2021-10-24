332b8f21a9c633418ea5b4495ac9519f0b936c32
Sahil Naik <81952895+Sahil-Naik@users.noreply.github.com>
2021-10-24 14:03:51 +0530

    Update Search_methods.py

--- Search_methods.py
+++ Search_methods.py
@@ -1,13 +1,20 @@
+# Types of Search method included are - Fibbonacci Search, Sentinel Search, Linear Search, Binary Search
+# This program perfroms various searching method on the student data given.
+# Student data included: Attendance record, Roll number, Student Names.
+
 import names
 import random
 
+# Initialized empty lists to store their respective values later in the code.
 stu_names = []
 stu_roll = []
 stu_att = []
+
+# Variable 'exit' and 'exile' both stores the condition of exiting for different stage of code.
 exit = False
 exile = False
-options = ['absent', 'present']
-quote = ['Hooray!', 'We did it!', 'Not bad!', 'Getting Serious?', 'No shaddy business.']
+options = ['absent', 'present'] # This list randomly assigns students as 'Absent' or 'Present'
+quote = ['Hooray!', 'We did it!', 'Not bad!', 'Getting Serious?', 'No shaddy business.'] # Just used to show random quotes on every run.
 
 # Function for Linear Search
 def lini_srch(na_me):
@@ -38,12 +45,16 @@ def senti_srch(lit, n, na_m):
 def binary_srch(arr_list, strt, end, var):
     if end >= strt:
         mid = (end + strt)//2
+        
         if arr_list[mid] == var:
             return mid
+        
         elif arr_list[mid] > var:
             return binary_srch(arr_list, strt, mid-1, var)
+        
         else:
             return binary_srch(arr_list, mid+1, end, var)
+        
     else:
         return print("No such Name found!")
 
@@ -52,24 +63,30 @@ def fibbo_srch(arr, x, n):
     fibMMm2 = 0  # (m-2)'th Fibonacci No.
     fibMMm1 = 1  # (m-1)'th Fibonacci No.
     fibM = fibMMm2 + fibMMm1
+    
     while (fibM < n):
         fibMMm2 = fibMMm1
         fibMMm1 = fibM
         fibM = fibMMm2 + fibMMm1
     offset = -1
+    
     while (fibM > 1):
         i = min(offset+fibMMm2, n-1)
+        
         if (arr[i] < x):
             fibM = fibMMm1
             fibMMm1 = fibMMm2
             fibMMm2 = fibM - fibMMm1
             offset = i
+            
         elif (arr[i] > x):
             fibM = fibMMm2
             fibMMm1 = fibMMm1 - fibMMm2
             fibMMm2 = fibM - fibMMm1
+            
         else:
             return i
+        
     if(fibMMm1 and arr[n-1] == x):
         return n-1
     return -1
@@ -86,23 +103,36 @@ gret = random.choice(quote)
 print(gret,len(stu_roll),"Students joined the training program.")
 
 # This code displays options for the user
+
+# Run program until prompted to exit by the user
 while exit != True:
     chc = int(input("List of options available :\n1 = Show names of student.\n2 = Show student's attendance.\n3 = Exit.\nEnter your Choice : "))
     if chc == 1:
         print("\nStudents who signed up for the program are :\n\n", stu_names,"\n")
+        
     elif chc == 2:
         while exile != True:
+            
+            # Variable 'huh' stores the name of student who's record is going to be checked
             huh = input("Enter the name of student : ").capitalize()
+            
+            # Variable 'ch1' stores user's choice for searching
             ch1 = int(input("Search using :\n1 = Linear Search\n2 = Sentinel Search\n3 = Binary Search\n4 = Fibonacci Search\n5 = Head back\nEnter your Choice : "))
+            
+            # Accept Search method from user and continue
+            
             if ch1 == 1:
                 lini_srch(huh)
+                
             elif ch1 == 2:
                 senti_srch(stu_names, len(stu_roll), huh)
+                
             elif ch1 == 3:
                 res = binary_srch(stu_names, 0, len(stu_roll)-1, huh)
                 indi = stu_names.index(huh)
                 if res != -1:
                     print(huh,"was", stu_att[indi],"for the training program.\n")
+                    
             elif ch1 == 4:
                 res = fibbo_srch(stu_names, huh, len(stu_roll))
                 indi = stu_names.index(huh)
@@ -110,8 +140,10 @@ while exit != True:
                     print(huh,"was", stu_att[indi],"for the training program.\n")
                 else:
                     print("No!")
+                    
             elif ch1 == 5:
-                exile = True        
+                exile = True    
+                
     elif chc == 3:
         print("\nSee you Soon!")
         exit = True
